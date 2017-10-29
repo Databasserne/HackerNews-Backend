9b38f00e28fc2da5926c4fec665cb18bb47d4f87
Martin Karlsen <1martinkarlsen@gmail.com>
2017-10-29 15:54:04 +0100

    feat(Sign Up): Resource finished

--- src/main/java/com/databasserne/hackernews/repo/impl/UserRepo.java
+++ src/main/java/com/databasserne/hackernews/repo/impl/UserRepo.java
@@ -3,9 +3,7 @@ package com.databasserne.hackernews.repo.impl;
 import com.databasserne.hackernews.model.User;
 import com.databasserne.hackernews.repo.IUserRepo;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.NoResultException;
+import javax.persistence.*;
 
 public class UserRepo implements IUserRepo {
 
@@ -34,6 +32,19 @@ public class UserRepo implements IUserRepo {
 
     @Override
     public User createUser(User user) {
-        return null;
+        em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+
+            return user;
+        } catch (EntityExistsException exist) {
+            throw new EntityExistsException();
+        } catch (RollbackException rollback) {
+            throw new EntityExistsException();
+        } finally {
+            em.close();
+        }
     }
 }
--- src/test/java/com/databasserne/hackernews/repo/UserRepoTest.java
+++ src/test/java/com/databasserne/hackernews/repo/UserRepoTest.java
@@ -5,6 +5,7 @@ import com.databasserne.hackernews.model.User;
 import com.databasserne.hackernews.repo.impl.UserRepo;
 import org.junit.*;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.Persistence;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -48,5 +49,27 @@ public class UserRepoTest {
         User user = userRepo.getUserByUsername(username);
 
         assertThat(user, is(nullValue()));
+    }
+
+    @Test
+    public void createUserSuccessTest() {
+        User user = new User();
+        user.setUsername("NewUser");
+        user.setPassword("1234");
+        user.setFullname("New User");
+
+        User resultUser = userRepo.createUser(user);
+        assertThat(resultUser.getUsername(), is("NewUser"));
+        assertThat(resultUser.getFullname(), is("New User"));
+    }
+
+    @Test (expected = EntityExistsException.class)
+    public void createUserAlreadyExistTest() {
+        User user = new User();
+        user.setUsername("TestUser");
+        user.setPassword("1234");
+        user.setFullname("Test User");
+
+        userRepo.createUser(user);;
     }
 }
