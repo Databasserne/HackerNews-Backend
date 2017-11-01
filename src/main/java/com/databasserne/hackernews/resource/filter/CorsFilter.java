098160f2d15aab0b89a4d663d9627272b47aa896
Martin Karlsen <1martinkarlsen@gmail.com>
2017-11-01 21:42:03 +0100

    Added to corsfilter

--- src/main/java/com/databasserne/hackernews/resource/filter/CorsFilter.java
+++ src/main/java/com/databasserne/hackernews/resource/filter/CorsFilter.java
@@ -20,9 +20,10 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
         response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-        response.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type,x-requested-with,Authorization");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE, HEAD");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "Origin,Content-Type,Accept,x-requested-with,Authorization");
         response.getHeaders().putSingle("Access-Control-Expose-Headers", "Content-Type,x-requested-with");
+        response.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
     }
     
 }
